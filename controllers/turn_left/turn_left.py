e6fb61840bc3d12d1bda3ec0219b87e420399efc
Ricardo Moran <richi.moran@gmail.com>
2022-03-28 11:23:13 -0300

    Added some controllers for testing

---
+++ controllers/turn_left/turn_left.py
@@ -0,0 +1,15 @@
+from controller import Robot
+
+TIME_STEP = 64
+
+robot = Robot()
+
+left_motor = robot.getDevice("left wheel motor")
+right_motor = robot.getDevice("right wheel motor")
+
+left_motor.setPosition(float('+inf'))
+right_motor.setPosition(float('+inf'))
+
+while robot.step(TIME_STEP) != -1:
+    left_motor.setVelocity(10.0)
+    right_motor.setVelocity(-10.0)
