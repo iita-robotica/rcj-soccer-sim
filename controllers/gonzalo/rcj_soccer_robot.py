6c638470ce585a24cf5aa492963b7bbdc4b269b5
Gonzalo Zabala <gzabala@gmail.com>
2022-03-31 16:20:56 -0300

    Controlador de Gonzalo

---
+++ controllers/gonzalo/rcj_soccer_robot.py
@@ -0,0 +1,292 @@
+import math
+import struct
+from utils import World
+from angles import r2d, d2r, normalize
+
+TIME_STEP = 64
+ROBOT_NAMES = ["B1", "B2", "B3", "Y1", "Y2", "Y3"]
+N_ROBOTS = len(ROBOT_NAMES)
+
+
+class RCJSoccerRobot:
+    def __init__(self, robot):
+        self.robot = robot
+        self.name = self.robot.getName()
+        self.team = self.name[0]
+        self.player_id = int(self.name[1])
+
+        self.receiver = self.robot.getDevice("supervisor receiver")
+        self.receiver.enable(TIME_STEP)
+
+        self.team_emitter = self.robot.getDevice("team emitter")
+        self.team_receiver = self.robot.getDevice("team receiver")
+        self.team_receiver.enable(TIME_STEP)
+
+        self.ball_receiver = self.robot.getDevice("ball receiver")
+        self.ball_receiver.enable(TIME_STEP)
+
+        self.gps = self.robot.getDevice("gps")
+        self.gps.enable(TIME_STEP)
+
+        self.compass = self.robot.getDevice("compass")
+        self.compass.enable(TIME_STEP)
+
+        self.sonar_left = self.robot.getDevice("distancesensor left")
+        self.sonar_left.enable(TIME_STEP)
+        self.sonar_right = self.robot.getDevice("distancesensor right")
+        self.sonar_right.enable(TIME_STEP)
+        self.sonar_front = self.robot.getDevice("distancesensor front")
+        self.sonar_front.enable(TIME_STEP)
+        self.sonar_back = self.robot.getDevice("distancesensor back")
+        self.sonar_back.enable(TIME_STEP)
+
+        self.left_motor = self.robot.getDevice("left wheel motor")
+        self.right_motor = self.robot.getDevice("right wheel motor")
+
+        self.left_motor.setPosition(float("+inf"))
+        self.right_motor.setPosition(float("+inf"))
+
+        self.left_motor.setVelocity(0.0)
+        self.right_motor.setVelocity(0.0)
+        
+        self.world=World()
+
+    def parse_supervisor_msg(self, packet: str) -> dict:
+        """Parse message received from supervisor
+
+        Returns:
+            dict: Location info about each robot and the ball.
+            Example:
+                {
+                    'waiting_for_kickoff': False,
+                }
+        """
+        # True/False telling whether the goal was scored
+        struct_fmt = "?"
+        unpacked = struct.unpack(struct_fmt, packet)
+
+        data = {"waiting_for_kickoff": unpacked[0]}
+        return data
+
+    def get_new_data(self) -> dict:
+        """Read new data from supervisor
+
+        Returns:
+            dict: See `parse_supervisor_msg` method
+        """
+        packet = self.receiver.getData()
+        self.receiver.nextPacket()
+
+        return self.parse_supervisor_msg(packet)
+
+    def is_new_data(self) -> bool:
+        """Check if there is new data from supervisor to be received
+
+        Returns:
+            bool: Whether there is new data received from supervisor.
+        """
+        return self.receiver.getQueueLength() > 0
+
+    def parse_team_msg(self, packet: str) -> dict:
+        """Parse message received from team robot
+
+        Returns:
+            dict: Parsed message stored in dictionary.
+        """
+        struct_fmt = "i"
+        unpacked = struct.unpack(struct_fmt, packet)
+        data = {
+            "robot_id": unpacked[0],
+            "x": unpacked[1],
+            "y": unpacked[2],
+            "rotation": unpacked[3],
+        }
+        return data
+
+    def get_new_team_data(self) -> dict:
+        """Read new data from team robot
+
+        Returns:
+            dict: See `parse_team_msg` method
+        """
+        packet = self.team_receiver.getData()
+        self.team_receiver.nextPacket()
+        return self.parse_team_msg(packet)
+
+    def is_new_team_data(self) -> bool:
+        """Check if there is new data from team robots to be received
+
+        Returns:
+            bool: Whether there is new data received from team robots.
+        """
+        return self.team_receiver.getQueueLength() > 0
+
+    def send_data_to_team(self) -> None:
+        """Send data to the team
+
+        Args:
+             robot_id (int): ID of the robot
+        """
+        struct_fmt = "i f f f"
+ 
+        data = [self.player_id, self.get_gps_coordinates()[0], self.get_gps_coordinates()[1], self.get_compass_heading()]#, self.world.getBall()[0], self.world.getBall()[1]]
+        print(self.world.getBall())
+        packet = struct.pack(struct_fmt, *data)
+        self.team_emitter.send(packet)
+
+    def get_new_ball_data(self) -> dict:
+        """Read new data from IR sensor
+
+        Returns:
+            dict: Direction and strength of the ball signal
+            Direction is normalized vector indicating the direction of the
+            emitter with respect to the receiver's coordinate system.
+            Example:
+                {
+                    'direction': [0.23, -0.10, 0.96],
+                    'strength': 0.1
+                }
+        """
+        _ = self.ball_receiver.getData()
+        data = {
+            "direction": self.ball_receiver.getEmitterDirection(),
+            "strength": self.ball_receiver.getSignalStrength(),
+        }
+
+        distancia=math.sqrt(1/data["strength"])
+        x=data["direction"][0]
+        y=data["direction"][1]
+        rx=self.get_gps_coordinates()[0]
+        ry=self.get_gps_coordinates()[1]
+        # print(rx,ry,x,y, distancia)
+        da=math.atan2(y,x)
+        da=normalize(da, -math.pi, math.pi)
+        # print(da, self.get_compass_heading())
+        a=self.get_compass_heading()+da
+        a=normalize(a, -math.pi, math.pi)
+        # print(r2d(self.get_compass_heading()), r2d(da), r2d(a))
+        
+        dx=math.sin(a)*distancia+rx
+        dy=(-math.cos(a)*distancia+ry)
+        
+        data={"x":dx, "y":dy}
+        # print(data, rx, ry)
+        self.ball_receiver.nextPacket()
+        return data
+
+    def is_new_ball_data(self) -> bool:
+        """Check if there is new data from ball to be received
+
+        Returns:
+            bool: Whether there is new data received from ball.
+        """
+        return self.ball_receiver.getQueueLength() > 0
+
+    def get_gps_coordinates(self) -> list:
+        """Get new GPS coordinates
+
+        Returns:
+            List containing x and y values
+        """
+        gps_values = self.gps.getValues()
+        return [gps_values[0], gps_values[1]]
+
+    def get_compass_heading(self) -> float:
+        """Get compass heading in radians
+
+        Returns:
+            float: Compass value in radians
+        """
+        compass_values = self.compass.getValues()
+
+        # Add math.pi/2 (90) so that the heading 0 is facing opponent's goal
+        rad = math.atan2(compass_values[0], compass_values[1]) + (math.pi / 2)
+        
+        rad=normalize(rad, -math.pi, math.pi)
+
+        return rad
+
+    def get_sonar_values(self) -> dict:
+        """Get new values from sonars.
+
+        Returns:
+            dict: Value for each sonar.
+        """
+        return {
+            "left": self.sonar_left.getValue(),
+            "right": self.sonar_right.getValue(),
+            "front": self.sonar_front.getValue(),
+            "back": self.sonar_back.getValue(),
+        }
+
+    def goToBall(self):
+        """Go to ball"""
+        if self.world.ball is None:
+            # If the robot does not see the ball, stop motors
+            self.left_motor.setVelocity(0)
+            self.right_motor.setVelocity(0)
+            return False
+
+        # Compute the speed for motors
+        direction = self.world.ball["direction"]
+
+        # If the robot has the ball right in front of it, go forward,
+        # rotate otherwise
+        if direction == 0:
+            left_speed = 5
+            right_speed = 5
+        else:
+            left_speed = direction * 4
+            right_speed = direction * -4
+
+        # Set the speed to motors
+        self.left_motor.setVelocity(left_speed)
+        self.right_motor.setVelocity(right_speed)
+        return True
+    
+    def goToPoint(self, x, y):
+        """Go to point x y"""
+        # Compute the speed for motors
+        direction = utils.get_direction(ball_data["direction"])
+
+        # If the robot has the ball right in front of it, go forward,
+        # rotate otherwise
+        if direction == 0:
+            left_speed = 5
+            right_speed = 5
+        else:
+            left_speed = direction * 4
+            right_speed = direction * -4
+
+        # Set the speed to motors
+        self.left_motor.setVelocity(left_speed)
+        self.right_motor.setVelocity(right_speed)
+        return True
+    
+    def stop(self):
+        self.left_motor.setVelocity(0)
+        self.right_motor.setVelocity(0)
+    
+    def setVelocity(self, vl, vr):
+        self.left_motor.setVelocity(vr)
+        self.right_motor.setVelocity(vl)
+        
+    def refreshWorld(self):
+        if self.is_new_data():
+            self.world.wfk= self.get_new_data()  # noqa: F841
+
+        while self.is_new_team_data():
+            team_data = self.get_new_team_data()
+            self.world.setRobot(team_data["robot_id"], team_data["x"], team_data["y"], team_data["rotation"]) 
+        
+
+        self.world.setRobot(self.player_id, self.get_gps_coordinates()[0], self.get_gps_coordinates()[1], self.get_compass_heading())  
+        
+        if self.is_new_ball_data():
+            ball_data = self.get_new_ball_data()
+            self.world.setBall(ball_data["x"], ball_data["y"])
+        else:
+            self.world.setBall(None, None)
+            
+    def run(self):
+        raise NotImplementedError
---
+++ controllers/gonzalo/utils.py
@@ -0,0 +1,34 @@
+def get_direction(ball_vector: list) -> int:
+    """Get direction to navigate robot to face the ball
+
+    Args:
+        ball_vector (list of floats): Current vector of the ball with respect
+            to the robot.
+
+    Returns:
+        int: 0 = forward, -1 = right, 1 = left
+    """
+    if -0.13 <= ball_vector[1] <= 0.13:
+        return 0
+    return -1 if ball_vector[1] < 0 else 1
+
+class World:
+    def __init__(self):
+        self.wfk=False
+        self.robots=[{"x":0, "y":0, "rot":0}, {"x":0, "y":0, "rot":0}, {"x":0, "y":0, "rot":0}]
+        self.ball={"x":0, "y":0}
+
+    def setRobot(self, id, x, y, rot):
+        self.robots[id]={"x":x, "y":y, "rot":rot}
+    
+    def getRobot(self, id):
+        return self.robots[id]
+    
+    def setBall(self, x, y):
+        self.ball={"x":x, "y":y}
+        
+    def getBall(self):
+        return self.ball
+
+
+    
